0ccfc0f0076383d8e365858c42cd43a65abfc32d
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-01-16 15:18:36 -0500

    Updated UI test for main functionality

--- spec/models/prediction_spec.rb
+++ spec/models/prediction_spec.rb
@@ -1,90 +1,15 @@
 require 'rails_helper'
 
-# NEED TO DRY THIS UP
-def create_concert_songs(new_show)
-
-    ConcertSong.create(
-      play_index: 1,
-      set_index: 1,
-      concert_id: new_show.id,
-      songs_in_set: 3
-    )
-
-    ConcertSong.create(
-    song_id: Song.find_by(song_name: "Suzy Greenberg").id,
-    play_index: 2,
-    set_index: 1,
-    concert_id: new_show.id,
-    songs_in_set: 3
-    )
-
-    ConcertSong.create(
-    song_id: Song.find_by(song_name: "David Bowie").id,
-    play_index: 3,
-    set_index: 1,
-    concert_id: new_show.id,
-    songs_in_set: 3
-    )
-
-    # second set
-
-    ConcertSong.create(
-    song_id: Song.find_by(song_name: "AC/DC Bag").id,
-    play_index: 1,
-    set_index: 2,
-    concert_id: new_show.id,
-    songs_in_set: 3
-    )
-
-    ConcertSong.create(
-    song_id: Song.find_by(song_name: "Run Like an Antelope").id,
-    play_index: 2,
-    set_index: 2,
-    concert_id: new_show.id,
-    songs_in_set: 3
-    )
-
-    ConcertSong.create(
-    song_id: Song.find_by(song_name: "Contact").id,
-    play_index: 3,
-    set_index: 2,
-    concert_id: new_show.id,
-    songs_in_set: 3
-    )
-
-    # encore
-    ConcertSong.create(
-    song_id: Song.find_by(song_name: "Chalk Dust Torture").id,
-    play_index: 1,
-    set_index: 3,
-    concert_id: new_show.id,
-    songs_in_set: 1
-    )
-
-end
-
-def create_concert_prediction(new_show)
-
-  Prediction.create(
-    user_id: 1,
-    concert_id: new_show.id,
-    set_one_opener_song_id: Song.find_by(song_name: "Harry Hood").id,
-    set_one_closer_song_id: Song.find_by(song_name: "David Bowie").id,
-    set_two_opener_song_id: Song.find_by(song_name: "AC/DC Bag").id,
-    set_two_closer_song_id: Song.find_by(song_name: "Contact").id,
-    encore_song_id: Song.find_by(song_name: "Chalk Dust Torture").id,
-    random_pick_song_id: Song.find_by(song_name: "Run Like an Antelope").id
-  )
-
-end
-
 RSpec.describe Prediction, :type => :model do
   it "allows a user to enter a valid prediction" do
-    new_show = FactoryGirl.build(:concert)
-    new_show.save!
+    prediction = Prediction.new
 
-    create_concert_songs(new_show)
-    prediction = create_concert_prediction(new_show)
+    prediction.set_one_opener_song_id = 3
+    prediction.set_one_closer_song_id = 1
+    prediction.set_two_opener_song_id = 2
+    prediction.set_two_closer_song_id = 2
+    prediction.encore_song_id = 4
+    prediction.random_pick_song_id = 5
 
     expect(prediction.save).to eq(true)
   end
@@ -101,17 +26,5 @@ RSpec.describe Prediction, :type => :model do
     expect(prediction.save).to eq(false)
   end
 
-  it "Calculates a prediction score for maximum points" do
 
-      new_show = FactoryGirl.build(:concert)
-      new_show.save!
-
-      create_concert_songs(new_show)
-      prediction = create_concert_prediction(new_show)
-      calculator = PredictionCalculator.new
-      calculator.get_prediction_score(prediction)
-      prediction.save!
-
-      expect(prediction.get_prediction_score).to eq(17)
-  end
 end
